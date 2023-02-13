7b9091542eae42937698746dba33bd2e13a0f62b
Zia123456 <m.ziabakhteyari12@gmail.com>
2023-02-14 01:37:58 +0530

    Create options

--- src/main.rb
+++ src/main.rb
@@ -1,3 +1,19 @@
 class Main
-  
+def initialize()
+    puts 'Welcome to out cateloge'
+    puts 'Please choose an option'
+    
+    option = 1
+
+when 1
+    #list of books
+when 2
+    #list of movies
+when 3
+    #list of games
+when 4
+    #list of musics
+when 5
+    exit
+
 end
